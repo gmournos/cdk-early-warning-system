3a3398103b7b4eb75218238c11673755d5b0420b
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-14 05:46:39 +0200

    7. Auto deploy the lambda function for retention

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const LIBRARY_NAMESPACE = 'ews';
 export const DEFAULT_RETENTION_FEATURE = `${LIBRARY_NAMESPACE}-cloudwatch-default-retention`;
 export const DEFAULT_RETENTION_FEATURE_STACK = `${DEFAULT_RETENTION_FEATURE}-stack`;
 export const DEFAULT_RETENTION_PARAM_KEY = `/${LIBRARY_NAMESPACE}/cloudwatch/logs/retetion`;
+export const DEFAULT_RETENTION_FEATURE_FUNCTION = `${DEFAULT_RETENTION_FEATURE}-function`;
+
--- lib/features/log-group-retention.ts
+++ lib/features/log-group-retention.ts
@@ -1,13 +1,38 @@
 import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
-import { DEFAULT_RETENTION_PARAM_KEY } from '../constants';
+import { DEFAULT_RETENTION_FEATURE_FUNCTION, DEFAULT_RETENTION_PARAM_KEY } from '../constants';
+import * as path from 'path';
 
 export class LogGroupRetentionStack extends NestedStack {
 
     constructor(scope: Construct, id: string, props?: NestedStackProps) {
         super(scope, id, props);
         const defaultRetentionParam = StringParameter.valueForStringParameter(this, DEFAULT_RETENTION_PARAM_KEY);
-        const defaultRetention = parseInt(defaultRetentionParam);
+        parseInt(defaultRetentionParam);
+        const setLogPolicy = new PolicyStatement({
+            actions: [
+                'logs:PutRetentionPolicy',  
+            ],
+            effect: Effect.ALLOW,
+            resources: [
+                '*',
+            ],
+        });
+
+        const setDefaultRetentionLambda = new NodejsFunction(this, DEFAULT_RETENTION_FEATURE_FUNCTION, {
+            functionName: DEFAULT_RETENTION_FEATURE_FUNCTION,
+            runtime: Runtime.NODEJS_20_X,
+            handler: 'setDefaultRetentionFromRule',
+            entry: path.join('lambda', 'log-groups', 'handlers.ts'),
+            environment: {
+                RETENTION_IN_DAYS : defaultRetentionParam,
+            },
+        });
+
+        setDefaultRetentionLambda.addToRolePolicy(setLogPolicy);
     }
 }
