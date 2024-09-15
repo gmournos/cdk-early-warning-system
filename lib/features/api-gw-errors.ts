0f69f20cf53b8dcb70ac2e3723d74a5cf0e3f2c5
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-09-15 08:06:26 +0200

    57 Add subscription filters that catch errors in api gw access logs

--- lib/features/api-gw-errors.ts
+++ lib/features/api-gw-errors.ts
@@ -1,10 +1,11 @@
-import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { Fn, NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ITopic } from 'aws-cdk-lib/aws-sns';
-import { ILogGroup } from 'aws-cdk-lib/aws-logs';
+import { FilterPattern, ILogGroup, LogGroup } from 'aws-cdk-lib/aws-logs';
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
 import { APIGW_ERRORS_FEATURE_FUNCTION } from '../constants';
 import { buildLogGroupForLambda, createLogSubscriptionAlertFunction } from '../utils/cloudwatch';
+import { LambdaDestination } from 'aws-cdk-lib/aws-logs-destinations';
 
 const FUNCTION_NAME = APIGW_ERRORS_FEATURE_FUNCTION;
 
@@ -22,6 +23,7 @@ export class ApiGatewayNotificationsStack extends NestedStack {
         super(scope, id, props);
         this.logGroup = buildLogGroupForLambda(this, FUNCTION_NAME);
         this.sendCustomizedNotificationFunction = this.createApiGatewayFailLambdaFunction(props.accountEnvironment, props.destinationTopic);
+        props.accessLogsArnRefs.forEach(logGroupRef => this.createAlertOnErrorStatusesForLogGroup(logGroupRef));
     }
 
     createApiGatewayFailLambdaFunction(accountEnvironment: string, destinationTopic: ITopic) {
@@ -33,6 +35,41 @@ export class ApiGatewayNotificationsStack extends NestedStack {
             handler: 'sendCustomizedNotificationFromApiGatewaySubscription',
             sourceFilePath: ['apigw', 'handlers.ts'],
             destinationTopic,
+        });
+    }
+    
+    createAlertOnErrorStatusesForLogGroup(logGroupArnRef: string) {
+        const logGroup = LogGroup.fromLogGroupArn(this, `lg-${logGroupArnRef}`, Fn.importValue(logGroupArnRef));
+
+        logGroup.addSubscriptionFilter(`${logGroupArnRef}-api-gw-subscription`, {
+            filterName: `${logGroupArnRef}-api-gw-error-status-filter`,
+            destination: new LambdaDestination(this.sendCustomizedNotificationFunction),
+            // listed at https://docs.aws.amazon.com/apigateway/latest/developerguide/supported-gateway-response-types.html
+            filterPattern: FilterPattern.anyTerm(
+                'ACCESS_DENIED', 
+                'API_CONFIGURATION_ERROR',
+                'AUTHORIZER_CONFIGURATION_ERROR',
+                'AUTHORIZER_FAILURE', 
+                'BAD_REQUEST_PARAMETERS',
+                'BAD_REQUEST_BODY',
+                'INTEGRATION_FAILURE', 
+                'INTEGRATION_TIMEOUT',
+                'INVALID_API_KEY',
+                'INTEGRATION_FAILURE', 
+                'INTEGRATION_TIMEOUT',
+                'INVALID_API_KEY',
+                'INVALID_SIGNATURE',
+                'MISSING_AUTHENTICATION_TOKEN',
+                'QUOTA_EXCEEDED',
+                'REQUEST_TOO_LARGE',
+                'RESOURCE_NOT_FOUND',
+                'THROTTLED', 
+                'UNAUTHORIZED',
+                'UNSUPPORTED_MEDIA_TYPE',
+                'WAF_FILTERED',
+                'DEFAULT_4XX',
+                'DEFAULT_5XX',
+            ), 
         });
     }
 }
