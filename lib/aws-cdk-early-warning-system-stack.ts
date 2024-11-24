3bb246aaadf916ba6b020129e61c38e7bbcb8c77
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-24 07:15:36 +0100

    3 Add feature stack 1. Default log retention

--- lib/aws-cdk-early-warning-system-stack.ts
+++ lib/aws-cdk-early-warning-system-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { LogGroupRetentionStack } from './features/log-group-retention';
+import { DEFAULT_RETENTION_FEATURE_STACK } from './constants';
 
 export class AwsCdkEarlyWarningSystemStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    new LogGroupRetentionStack(this, DEFAULT_RETENTION_FEATURE_STACK, props);
   }
 }
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const LIBRARY_NAMESPACE = 'ews';
+
+export const DEFAULT_RETENTION_FEATURE = `${LIBRARY_NAMESPACE}-cloudwatch-default-retention`;
+export const DEFAULT_RETENTION_FEATURE_STACK = `${DEFAULT_RETENTION_FEATURE}-stack`;
