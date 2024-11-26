3f91e2af8016670a7f9053f5ad81360212cf5004
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-26 07:37:24 +0100

    35 Auto deploy code for etl summary lambda

--- lib/features/glue-etl-summary.ts
+++ lib/features/glue-etl-summary.ts
@@ -1,16 +1,20 @@
-import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Duration, NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { ILogGroup } from 'aws-cdk-lib/aws-logs';
 import { ITopic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 import { buildLogGroupForLambda } from '../utils/cloudwatch';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import { GLUE_FAILURE_SUMMARY_FEATURE_FUNCTION } from '../constants';
 
 export interface GlueSummaryStackProps extends NestedStackProps {
     destinationTopic: ITopic;
     accountEnvironment: string;
 }
 
-const FUNCTION_NAME = 'etl-summary-function';
+const FUNCTION_NAME = GLUE_FAILURE_SUMMARY_FEATURE_FUNCTION;
 
 export class GlueSummaryStack extends NestedStack {
     failedEtlFunction: IFunction;
@@ -19,5 +23,44 @@ export class GlueSummaryStack extends NestedStack {
     constructor(scope: Construct, id: string, props: GlueSummaryStackProps) {
         super(scope, id, props);
         this.logGroup = buildLogGroupForLambda(this, FUNCTION_NAME);
+    }
+
+    createSummaryEtlLambdaFunction(accountEnvironment: string, destinationTopic: ITopic) {
+
+        const sendToTopicPolicy = new PolicyStatement({
+            actions: [
+                'sns:Publish',
+            ],
+            effect: Effect.ALLOW,
+            resources: [
+                destinationTopic.topicArn,
+            ],
+        });
+
+        const gluePolicy = new PolicyStatement({
+            actions: [
+                'glue:GetJobs',
+                'glue:GetJobRuns',
+            ],
+            effect: Effect.ALLOW,
+            resources: [ '*'],
+        });
+
+        const sendCustomizedNotificationForEtlSummary = new NodejsFunction(this, GLUE_FAILURE_SUMMARY_FEATURE_FUNCTION, {
+            functionName: FUNCTION_NAME,
+            logGroup : this.logGroup,
+            runtime: Runtime.NODEJS_20_X,
+            handler: 'findLastHoursNoSuccessfulRun',
+            entry: path.join('lambda', 'glue', 'handlers.ts'),
+            timeout: Duration.seconds(120), 
+            environment: {
+                TOPIC_ARN : destinationTopic.topicArn,
+                ACCOUNT_ENVIRONMENT: accountEnvironment.toUpperCase(),
+            },
+        });
+
+        sendCustomizedNotificationForEtlSummary.addToRolePolicy(sendToTopicPolicy);
+        sendCustomizedNotificationForEtlSummary.addToRolePolicy(gluePolicy);
+        return sendCustomizedNotificationForEtlSummary;
     }
 }
