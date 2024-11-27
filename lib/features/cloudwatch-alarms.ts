209f47c83fab98f994022c72956ab49019a3348b
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-27 06:06:39 +0100

    44 Create auto deploy code for lambda

--- lib/features/cloudwatch-alarms.ts
+++ lib/features/cloudwatch-alarms.ts
@@ -1,10 +1,13 @@
 import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { ILogGroup } from 'aws-cdk-lib/aws-logs';
 import { ITopic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 import { buildLogGroupForLambda } from '../utils/cloudwatch';
 import { CLOUDWATCH_ALARMS_FEATURE_FUNCTION } from '../constants';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export interface NotificationsOnAlarmsStackProps extends NestedStackProps {
     destinationTopic: ITopic;
@@ -20,6 +23,35 @@ export class NotificationsOnAlarmsStack extends NestedStack {
     constructor(scope: Construct, id: string, props: NotificationsOnAlarmsStackProps) {
         super(scope, id, props);
         this.logGroup = buildLogGroupForLambda(this, FUNCTION_NAME);
+        this.customNotificationsOnAlarms = this.createLambdaFunction(props.accountEnvironment, props.destinationTopic);
+    }
+
+    createLambdaFunction(accountEnvironment: string, destinationTopic: ITopic) {
+
+        const sendToTopicPolicy = new PolicyStatement({
+            actions: [
+                'sns:Publish',
+            ],
+            effect: Effect.ALLOW,
+            resources: [
+                destinationTopic.topicArn,
+            ],
+        });
+
+        const sendCustomizedNotificationFromAlarm = new NodejsFunction(this, FUNCTION_NAME, {
+            functionName: FUNCTION_NAME,
+            logGroup: this.logGroup,
+            runtime: Runtime.NODEJS_20_X,
+            handler: 'sendCustomizedNotificationFromAlarm',
+            entry: path.join('lambda', 'alarms', 'handlers.ts'),
+            environment: {
+                TOPIC_ARN : destinationTopic.topicArn,
+                ACCOUNT_ENVIRONMENT: accountEnvironment.toUpperCase(),
+            },
+        });
+
+        sendCustomizedNotificationFromAlarm.addToRolePolicy(sendToTopicPolicy);
+        return sendCustomizedNotificationFromAlarm;
     }
 
 }
